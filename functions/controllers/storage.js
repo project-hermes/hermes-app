1b51be246d7e5ddf7ed636394717d54986cb7e50
James Wilson <jwils22@gmail.com>
2018-10-07 20:32:52 -0400

    Built readFile for Storage Controller

--- functions/controllers/storage.js
+++ functions/controllers/storage.js
@@ -1,7 +1,24 @@
-const StorageController = function () {
+const os = require('os');
+const path = require('path');
 
-    const readFile = function (storageObject) {
-        
+const StorageController = function (storageInt, fs) {
+
+    this.storageInt = storageInt;
+
+    const readFile = function* (storageObject) {
+        // const filePath = bucketObject.name;
+        const parsedPath = path.parse(storageObject.name);
+        const fileName = parsedPath.base;
+    
+        const bucket = storageInt.bucket(storageObject.bucket);
+        const file = bucket.file(storageObject.name);
+
+        const tempLocalDir = path.join(os.tmpdir(), parsedPath.dir);
+        const tempLocalFile = path.join(tempLocalDir, fileName);
+
+        const fileDownload = yield file.download({ destination: tempLocalFile});
+
+        return fileDownload;
     };
 
     return {
