130a0c21b85e48ce6f86957067665b9f2c8c4591
Bradford Bazemore <bankersalgo@gmail.com>
2018-10-08 23:09:19 -0400

    it can read

--- functions/index.js
+++ functions/index.js
@@ -5,7 +5,7 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 const path = require('path');
 const os = require('os');
-const fs = require('fs');
+const convert = require('xml-js');
 
 exports.processDivelog = functions.storage.object().onFinalize(async object => {
     const fileBucket = object.bucket;
@@ -13,9 +13,12 @@ exports.processDivelog = functions.storage.object().onFinalize(async object => {
     const contentType = object.contentType;
     const metageneration = object.metageneration;
 
-    if (!contentType.startsWith('xml')) {
+    if (!contentType.endsWith('xml')) {
         return console.log(
-            filePath + ' is not an xml file. We will not process this.'
+            filePath +
+                ' with ' +
+                contentType +
+                'is not an xml file. We will not process this.'
         );
     }
     const fileName = path.basename(filePath);
@@ -25,7 +28,25 @@ exports.processDivelog = functions.storage.object().onFinalize(async object => {
     const metadata = {
         contentType: contentType
     };
-    await bucket.file(filePath).download({destination: tempFilePath});
-    console.log('Divelog downloaded locally to', tempFilePath);
-    return fs.unlinkSync(tempFilePath);
+    var data = await bucket.file(filePath).download();
+    console.log('Divelog loaded');
+    var diveData = convert.xml2js(data.toString(), {
+        compact: true,
+        spaces: 4
+    });
+    console.log('Converted dive log');
+    for (var d in diveData.dives.dive) {
+        console.log(
+            diveData.dives.dive[d].date._text,
+            diveData.dives.dive[d].duration._text,
+            diveData.dives.dive[d].maxDepth._text,
+            diveData.dives.dive[d].averageDepth._text,
+            diveData.dives.dive[d].tempHigh._text,
+            diveData.dives.dive[d].tempLow._text,
+            diveData.dives.dive[d].site.lat,
+            diveData.dives.dive[d].site.lon,
+            diveData.dives.dive[d].gear.item.name._text,
+            diveData.dives.dive[d].gear.item.serial._text
+        );
+    }
 });
