c02dafaeee3294dc1740345d8bc69b72b5b4e9b5
Bradford Bazemore <bankersalgo@gmail.com>
2018-10-08 21:51:37 -0400

    i am trying

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,31 @@
+'use strict';
+
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.processDivelog = functions.storage.object().onFinalize(async object => {
+    const fileBucket = object.bucket;
+    const filePath = object.name;
+    const contentType = object.contentType;
+    const metageneration = object.metageneration;
 
+    if (!contentType.startsWith('xml')) {
+        return console.log(
+            filePath + ' is not an xml file. We will not process this.'
+        );
+    }
+    const fileName = path.basename(filePath);
 
+    const bucket = admin.storage().bucket(fileBucket);
+    const tempFilePath = path.join(os.tmpdir(), fileName);
+    const metadata = {
+        contentType: contentType
+    };
+    await bucket.file(filePath).download({destination: tempFilePath});
+    console.log('Divelog downloaded locally to', tempFilePath);
+    return fs.unlinkSync(tempFilePath);
+});
