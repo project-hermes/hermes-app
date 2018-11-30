2ead314cab700bf0e0c8692f7d3dd9a1ab3368b7
James Wilson <jwils22@gmail.com>
2018-11-29 20:09:02 -0500

    Changed to Go 1.11

--- server/services/main.go
+++ server/services/main.go
@@ -1,19 +1,60 @@
-package services
+package main
 
 import (
-	"google.golang.org/appengine"
-	"hermes/server"
+	"fmt"
+	"html/template"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
+type templateParams struct {
+	Notice string
+	Name string
+}
+
+var (
+	indexTemplate = template.Must(template.ParseFiles("index.html"))
+)
+
+func init() {
+
+}
+
 func main() {
-	router := gin.New()
+	router := gin.Default()
 	router.Use(gin.ErrorLogger())
 	router.Use(gin.Recovery())
-	server.ApiEntry(router)
-	http.Handle("/", router)
 
-	appengine.Main()
+	appGroup := router.Group("/")
+	appGroup.GET("/", helloForm)
+	appGroup.POST("/", basicPostForm)
+	
+	router.Run()
+}
+
+func helloForm(c *gin.Context) {
+	params := templateParams{}
+	indexTemplate.Execute(c.Writer, params)
+	return
+}
+
+func basicPostForm(c *gin.Context) {
+	params := templateParams{}
+	name := c.Request.FormValue("name")
+	if name == "" {
+		name = "Anonymous Gopher"
+	} else {
+		params.Name = name
+	}
+
+	if c.Request.FormValue("message") == "" {
+		c.Writer.WriteHeader(http.StatusBadRequest)
+		params.Notice = "No message provided"
+		indexTemplate.Execute(c.Writer, params)
+		return
+	}
+
+	params.Notice = fmt.Sprintf("Thank you for your submission, %s!", name)
+	indexTemplate.Execute(c.Writer, params)
 }
